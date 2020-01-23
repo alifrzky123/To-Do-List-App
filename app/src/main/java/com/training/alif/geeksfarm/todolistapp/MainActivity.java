3aa38589d510d1da56f2b493e860194b453e8312
alifrizky <alifrzky78@gmail.com>
2020-01-23 12:26:56 +0700

    bisa menggunakan dialog alert didalam fab dan removing

--- app/src/main/java/com/training/alif/geeksfarm/todolistapp/MainActivity.java
+++ app/src/main/java/com/training/alif/geeksfarm/todolistapp/MainActivity.java
@@ -1,9 +1,10 @@
 package com.training.alif.geeksfarm.todolistapp;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -20,66 +21,72 @@ public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<String> itemsAdapter;
     private ListView lvItems;
     private FloatingActionButton fba;
-    private EditText etNewItem;
-    private static final String TAG = "MainActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initial();
-        fba.setOnClickListener(new View.OnClickListener() {
+        lvItems.setAdapter(itemsAdapter);
 
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final int which_pos = position;
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Have you done finished ")
+                        .setMessage("Beneran sudah dikerjakan?")
+                        .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                items.remove(which_pos);
+                                itemsAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("Belum" , null).show();
+
+                return false;
+            }
+        });
+
+        fba.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                items = new ArrayList<String>();
-                itemsAdapter = new ArrayAdapter<String>(MainActivity.this,
-                        android.R.layout.simple_list_item_1, items);
-                lvItems.setAdapter(itemsAdapter);
-                dialogs();
+                addItems();
             }
         });
     }
 
     public void initial() {
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, items);
         lvItems = (ListView) findViewById(R.id.lvToDoList);
         fba = (FloatingActionButton) findViewById(R.id.btn_fba);
     }
 
-    public void dialogs(){
+    public void addItems(){
         final EditText taskEditText = new EditText(MainActivity.this);
         AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
                 .setTitle("Add a new task to do")
                 .setMessage("What do you want to do next?")
                 .setView(taskEditText)
                 .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String task = String.valueOf(taskEditText.getText());
-                        Log.d(TAG,"Task to add: " + task);
-                        itemsAdapter.add("halo");
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                        items.add(taskEditText.getText().toString());
+                        itemsAdapter.notifyDataSetChanged();
                     }
                 })
                 .setNegativeButton("Cancel", null)
                 .create();
         dialog.show();
     }
+    public void addToSh(){
+        SharedPreferences share = getSharedPreferences("to do" , MODE_PRIVATE);
+        SharedPreferences.Editor Edit = share.edit();
+        String k = String.valueOf(key);
+        edit.putString(k, items);
+        edit.apply();
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_add_task:
-                Log.d(TAG, "Add a new task");
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }*/
 }
